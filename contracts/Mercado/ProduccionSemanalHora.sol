450f682ed11a57a26eef736bbf32513b1d55209d
tfgpgalan <tfgpgalan@gmail.com >
2023-02-25 20:36:40 +0100

    Produccion Semanal

--- contracts/Mercado/ProduccionSemanalHora.sol
+++ contracts/Mercado/ProduccionSemanalHora.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.15;
 
 import "./IERC20.sol";
 import "./MapIterLib.sol";
+import "./BokkyPooBahsDateTimeLibrary.sol";
  
  
 contract Produccion is IERC20 {
@@ -10,35 +11,57 @@ contract Produccion is IERC20 {
     uint256 public totalSupply;
     mapping(address => uint256) private _balance;
     mapping(address => mapping(address => uint256)) private _allowance;
+    //Balance de cada address por día de la semana y dentro de este por cada hora del día
+    mapping(address => mapping(uint => mapping (uint => uint256))) private _balanceHoraDayofWeek;
     uint8 public decimals;
     string public name;
     uint256 private constant MAX_UINT256 = 2**256 - 1;
     address owner;
+    uint fechaInicio;
     //Utiles para mantener la lista de productores iterable,
     //Todo productor que alguna vez haya producido o recibido se registrará
     //en _productores.
     using MapIterableLib for MapIterableLib.MapIterable;
     MapIterableLib.MapIterable private _productores;
+    using BokkyPooBahsDateTimeLibrary for uint;
 
 
+    event GrabaHora(address indexed from, uint dia, uint hora, uint produccion);
 
-    constructor(uint256 _initialAmount, string memory _tokenName, uint8 _decimalUnits
-    ) {
+    constructor(uint256 _initialAmount, string memory _tokenName, uint8 _decimalUnits) {
         _balance[msg.sender] = _initialAmount;
         totalSupply = _initialAmount;
         name = _tokenName;
         decimals = _decimalUnits;
         _productores.inicia();
         owner = msg.sender;
+        fechaInicio=block.timestamp;
+        
     }
 
+
+    function mes() public view returns (uint _mes){
+        uint ahora=block.timestamp;
+        
+        return ahora.getMonth();
+    }
+//0x0000000000000000000000000000000000000000
     //Si _to es 0x0 se supone que es producción de watios
     function transfer(address _to, uint256 _value) public override returns (bool success)
     {
         if (_to == address(0)) {
+            if (!_productores.isInList(msg.sender)){
+                _productores.addElemento(msg.sender);
+                inicia_balance_productor_nuevo(msg.sender);
+            }
             _balance[msg.sender] += _value;
+            uint dayofWeek=block.timestamp.getDayOfWeek();
+            uint horadeldia=block.timestamp.getHour();
+            if (horadeldia==0) iniciaDia(dayofWeek,msg.sender);
+            emit GrabaHora(msg.sender, dayofWeek-1, horadeldia,_value);
+            _balanceHoraDayofWeek[msg.sender][dayofWeek-1][horadeldia]+=_value;
             totalSupply+=_value;
-            _productores.addElemento(msg.sender);
+
         } else {
             require(_balance[msg.sender] >= _value, "Valor a transferir superior al existente");
             _balance[msg.sender] -= _value;
@@ -49,6 +72,43 @@ contract Produccion is IERC20 {
         return true;
     }
 
+    function inicia_balance_productor_nuevo(address _address) internal{
+        for (uint dia=1; dia<=7;dia++){
+           iniciaDia(dia,_address);
+        }
+    }
+//Función para generar valores demo, quitar en producción
+    function acumula_dia(address _address,uint _dayOfWeek,uint[] memory _produccion_hora) public {
+        require(_dayOfWeek>=1 && _dayOfWeek<=7 && _produccion_hora.length==24,"Día o matriz de horas(24) error");
+        if (!_productores.isInList(_address)){
+            _productores.addElemento(_address);
+            inicia_balance_productor_nuevo(_address);
+        }
+        _productores.addElemento(_address);
+        for(uint i = 0; i<24; i++) { 
+            _balanceHoraDayofWeek[_address][_dayOfWeek-1][i]+=_produccion_hora[i];
+        }
+
+    }
+
+    function iniciaDia(uint _dia,address _address) internal{
+        for(uint i = 0; i<24; i++) { 
+        _balanceHoraDayofWeek[_address][_dia-1][i]=0;
+        }
+    }
+
+    function getBalanceOfDaysOfWeek(address _productor) public view returns(uint256[24][7] memory elementos) {
+        uint256[24][7] memory elementosArray ;
+        for (uint dia=0; dia<=6;dia++){
+            for (uint hora=0; hora <=23; hora++){
+                elementosArray[dia][hora]= _balanceHoraDayofWeek[_productor][dia][hora];
+            }
+        }
+        return elementosArray;
+    }
+
+ 
+    
     
     function transferFrom(address _from, address _to,uint256 _value) public override returns (bool success) {
         uint256 _allowanced = _allowance[_from][msg.sender];
